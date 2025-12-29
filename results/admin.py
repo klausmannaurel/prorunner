221899753c9ad4831fc006233c1b05f5d8ab72b4
klausmannaurel <95487212+klausmannaurel@users.noreply.github.com>
2025-12-29 16:33:01 +0100

    Update admin.py

--- results/admin.py
+++ results/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
-from .models import Track, Result, Profile  # Itt hozzáadtuk a Profile-t!
+# Fontos: Itt adtuk hozzá a TrackReview-t a listához!
+from .models import Track, Result, Profile, TrackReview
 
 # --- 1. PROFIL BEÁGYAZÁSA A USER ADMINBA ---
 
@@ -21,7 +22,7 @@ admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 
 
-# --- 2. A TE MEGLÉVŐ BEÁLLÍTÁSAID (Változatlanul) ---
+# --- 2. EGYÉB MODELLEK ADMINISZTRÁCIÓJA ---
 
 class TrackAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'distance_km_per_lap', 'surface_type')
@@ -31,8 +32,24 @@ class ResultAdmin(admin.ModelAdmin):
     list_filter = ('track', 'recorded_at')
     search_fields = ('runner_name', 'lap_times')
 
-# A saját modellek regisztrálása
+# --- EZ AZ ÚJ RÉSZ: VÉLEMÉNYEK KEZELÉSE ---
+class TrackReviewAdmin(admin.ModelAdmin):
+    # Ezeket az oszlopokat látod majd a listában:
+    list_display = ('user', 'track', 'rating', 'created_at')
+    
+    # Ezek alapján tudsz szűrni oldalt:
+    list_filter = ('rating', 'track', 'created_at')
+    
+    # Itt tudsz keresni (név, pálya, szöveg):
+    search_fields = ('user__username', 'track__name', 'comment')
+    
+    # A dátumot csak olvasni engedjük, nehogy véletlenül átírd:
+    readonly_fields = ('created_at',)
+
+# Modellek regisztrálása
 admin.site.register(Track, TrackAdmin)
 admin.site.register(Result, ResultAdmin)
+admin.site.register(TrackReview, TrackReviewAdmin) # <--- Itt aktiváltuk az új admin felületet!
+
 # Ha a Profilokat külön listában is látni akarod, vedd ki a kommentet:
 # admin.site.register(Profile)
